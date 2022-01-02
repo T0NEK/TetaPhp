6140adcfab50a34fccdc4122d2c237636c8f8a3d
T0NEK <88771701+T0NEK@users.noreply.github.com>
2022-01-02 23:32:10 +0100

    20220102

---
+++ teta(1).sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 02 Sty 2022, 23:27
+-- Wersja serwera: 10.1.21-MariaDB
+-- Wersja PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `teta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ustawienia`
+--
+
+CREATE TABLE `ustawienia` (
+  `id` int(11) NOT NULL,
+  `nazwa` text COLLATE utf8_polish_ci NOT NULL,
+  `opis` text COLLATE utf8_polish_ci NOT NULL,
+  `wartosc` text COLLATE utf8_polish_ci NOT NULL,
+  `wartosc1` text COLLATE utf8_polish_ci NOT NULL,
+  `wartosc2` text COLLATE utf8_polish_ci NOT NULL,
+  `wartosc3` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
+
+--
+-- Zrzut danych tabeli `ustawienia`
+--
+
+INSERT INTO `ustawienia` (`id`, `nazwa`, `opis`, `wartosc`, `wartosc1`, `wartosc2`, `wartosc3`) VALUES
+(1, 'Oryginalna data startu Dedala', '', '2043-03-11 12:01:02', '', '', ''),
+(2, 'Obecna data startu Dedala', '', '2022-01-01 12:00:00', '', '', ''),
+(3, 'Oryginalna data startu akcji na Dedalu', '', '2045-04-18 21:35:00', '', '', ''),
+(4, 'Obecna data akcji na Dedalu', '', '2022-01-02 16:00:00', '', '', ''),
+(5, 'Stan larpa', '', 'START', 'START', '', '');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `ustawienia`
+--
+ALTER TABLE `ustawienia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `ustawienia`
+--
+ALTER TABLE `ustawienia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
