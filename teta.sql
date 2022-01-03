86877192d0d7c987cfb1c8277de305ef01a6b087
T0NEK <88771701+T0NEK@users.noreply.github.com>
2022-01-03 22:29:35 +0100

    20220103 dom 2

--- teta.sql
+++ teta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 02 Sty 2022, 23:31
+-- Czas generowania: 03 Sty 2022, 22:27
 -- Wersja serwera: 10.1.21-MariaDB
 -- Wersja PHP: 5.6.30
 
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `ustawienia` (
   `id` int(11) NOT NULL,
   `nazwa` text COLLATE utf8_polish_ci NOT NULL,
-  `opis` text COLLATE utf8_polish_ci NOT NULL,
   `wartosc` text COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
 
@@ -37,12 +36,13 @@ CREATE TABLE `ustawienia` (
 -- Zrzut danych tabeli `ustawienia`
 --
 
-INSERT INTO `ustawienia` (`id`, `nazwa`, `opis`, `wartosc`) VALUES
-(1, 'Oryginalna data startu Dedala', '', '2043-03-11 12:01:02'),
-(2, 'Obecna data startu Dedala', '', '2022-01-01 12:00:00'),
-(3, 'Oryginalna data startu akcji na Dedalu', '', '2045-04-18 21:35:00'),
-(4, 'Obecna data akcji na Dedalu', '', '2022-01-02 16:00:00'),
-(5, 'Stan larpa', '', 'START');
+INSERT INTO `ustawienia` (`id`, `nazwa`, `wartosc`) VALUES
+(1, 'Oryginalna data startu Dedala', '2043-03-11 12:01:02'),
+(2, 'Obecna data startu Dedala', '2043-03-11 12:01:02'),
+(3, 'Oryginalna data startu akcji na Dedalu', '2045-04-18 21:35:00'),
+(4, 'Obecna data akcji na Dedalu', '2022-01-12 12:00:00'),
+(5, 'Stan larpa', 'START'),
+(6, 'Data startu larpa', '2022-01-03 20:34:34');
 
 --
 -- Indeksy dla zrzutów tabel
