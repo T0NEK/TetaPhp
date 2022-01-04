d116842c81829c3d252ddd11f0c081ef1e99f985
Tomasz Kwarciński <vit@pro.onet.pl>
2022-01-04 15:04:10 +0100

    20220104 praca 1

--- teta.sql
+++ teta.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 03 Sty 2022, 22:27
--- Wersja serwera: 10.1.21-MariaDB
--- Wersja PHP: 5.6.30
+-- Czas generowania: 04 Sty 2022, 15:02
+-- Wersja serwera: 10.1.28-MariaDB
+-- Wersja PHP: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `ustawienia` (
   `id` int(11) NOT NULL,
+  `zmiana` text COLLATE utf8_polish_ci NOT NULL,
   `nazwa` text COLLATE utf8_polish_ci NOT NULL,
   `wartosc` text COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
@@ -36,13 +39,13 @@ CREATE TABLE `ustawienia` (
 -- Zrzut danych tabeli `ustawienia`
 --
 
-INSERT INTO `ustawienia` (`id`, `nazwa`, `wartosc`) VALUES
-(1, 'Oryginalna data startu Dedala', '2043-03-11 12:01:02'),
-(2, 'Obecna data startu Dedala', '2043-03-11 12:01:02'),
-(3, 'Oryginalna data startu akcji na Dedalu', '2045-04-18 21:35:00'),
-(4, 'Obecna data akcji na Dedalu', '2022-01-12 12:00:00'),
-(5, 'Stan larpa', 'START'),
-(6, 'Data startu larpa', '2022-01-03 20:34:34');
+INSERT INTO `ustawienia` (`id`, `zmiana`, `nazwa`, `wartosc`) VALUES
+(1, '', 'Oryginalna data startu Dedala', '2043-03-11 12:01:02'),
+(2, '2022-01-04 14:59:55', 'Obecna data startu Dedala', '2059-06-05 12:00:00'),
+(3, '', 'Oryginalna data startu akcji na Dedalu', '2045-04-18 21:35:00'),
+(4, '2022-01-04 14:56:41', 'Obecna data akcji na Dedalu', '2045-04-18 21:35:00'),
+(5, '2022-01-04 14:56:41', 'Stan larpa', 'START'),
+(6, '2022-01-04 14:56:41', 'Data startu larpa ', '2022-01-04 14:56:41');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -63,6 +66,8 @@ ALTER TABLE `ustawienia`
 --
 ALTER TABLE `ustawienia`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
